fb3565a998e1177f3a6a1f6cef63a23ee519a951
Paola Guitian <poliguitian@gmail.com>
2020-07-14 16:25:40 -0400

    add react and app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      Restaurant Picker
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
