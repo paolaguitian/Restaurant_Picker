4fb2016c2b3e86169b5c5ffd6be98676ac89d77e
Paola Guitian <poliguitian@gmail.com>
2020-07-23 16:49:04 -0400

    form functionality

---
+++ src/search/Search.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import './Search.css'
+import logo from '../logo.png'
+import { Form, Input, Button } from 'antd';
+
+
+const Search = () => {
+  const [food, setFood] = useState('');
+  const [location, setLocation] = useState('');
+  const [submit, setSubmit] = useState(false);
+
+  const onFinish = (values) => {
+    console.log(values)
+  }
+
+  return (
+    <div className="search-container">
+      <div className="title">
+        <img className="icon" src={logo} />
+        IT 4 ME
+      </div>
+      <div className="form">
+        <Form onFinish={onFinish}>
+          <Form.Item>
+            <Input.Group size="large" compact>
+              <Form.Item
+                name={['food']}
+                noStyle
+              >
+                <Input style={{ width: '50%' }} placeholder="burgers,thai,tacos,american..." />
+              </Form.Item>
+              <Form.Item
+                name={['location']}
+                noStyle
+              >
+                <Input style={{ width: '50%' }} placeholder="Input street" />
+              </Form.Item>
+            </Input.Group>
+          </Form.Item>
+          <Button className="submit" type="primary" htmlType="submit">
+            Submit
+        </Button>
+        </Form>
+      </div>
+    </div >
+  )
+}
+export default Search;
